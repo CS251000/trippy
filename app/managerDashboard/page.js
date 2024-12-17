85c33008e4df5801e371effbbf6270b686ef188b
Tanay <tanayrox2004@gmail.com>
2024-12-18 00:22:42 +0530

    tanay

--- app/managerDashboard/page.js
+++ app/managerDashboard/page.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function ManagerDashboard() {
-  return (
-    <div>
-      Manager dashboard by me
-    </div>
-  )
+  return <div>Manager dashboard by Tanay</div>;
 }
