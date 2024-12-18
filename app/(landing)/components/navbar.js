5a8e9fadc763f37e475bb6faf88bb41124ce572a
ved015 <vedant.04.mahajan@gmail.com>
2024-12-18 12:08:44 +0530

    navbar updated vedant

--- app/(landing)/components/navbar.js
+++ app/(landing)/components/navbar.js
@@ -1,21 +1,40 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export const NavbarPage = () => {
-    return(
-        <div className="flex flex-row space-x-4">
-            <div> Navbar</div>
-            <div>
-                <Link href={'/sign-up'}>
-                <button className="bg-white text-black block">Signup</button>
+    return (
+        <div className="sticky top-0 z-50 flex justify-between items-center px-8 py-4 bg-black text-white font-mono shadow-lg">
+            <div className="flex items-center space-x-2">
+                <Image src="/logo.png" width={40} height={30} alt="logo" />
+                <Link href="/" scroll={true}>
+                    <div className="text-xl font-bold cursor-pointer">Trippy</div>
                 </Link>
             </div>
+
+            <div className="flex space-x-8">
+                <Link href="/home" className="hover:text-gray-400">
+                    Home
+                </Link>
+                <Link href="/about-us" className="hover:text-gray-400">
+                    About Us
+                </Link>
+                <Link href="/places" className="hover:text-gray-400">
+                    Places
+                </Link>
+                <Link href="/services" className="hover:text-gray-400">
+                    Services
+                </Link>
+            </div>
+
             <div>
-                <Link href={'/sign-in'}>
-                <button className="bg-white text-black block">Sign in</button>
+                <Link href="/contact-us">
+                    <button className="bg-white text-black px-4 py-2 rounded hover:bg-gray-300 font-semibold">
+                        Contact Us
+                    </button>
                 </Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default NavbarPage;
